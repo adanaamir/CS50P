040c1160524f99e017da25f8c710f7c3f37f2bf5
adanaamir <adanaamir2005@gmail.com>
2024-08-04 00:07:51 -0700

    .

---
+++ ProblemSet_6/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+A":
+#     muppet = muppet.convert("RGBA")
+# if resized_shirt.mode != "RGBA":
+#     resized_shirt = resized_shirt.convert("RGBA")
+
+# position = (300,450)
+# #after resizing, overlaying the image 
+# muppet.paste(resized_shirt, position, resized_shirt) 
+
+# #saving the resized image as png file to save the transparency
+# muppet.save("after1
