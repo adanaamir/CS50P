24e79b0010c56c4a3506efdd956314b4744f2e6e
adanaamir <adanaamir2005@gmail.com>
2024-08-06 19:15:09 -0700

    .

---
+++ ProblemSet_7/um.py
@@ -0,0 +1,12 @@
+import re
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    match =  re.findall(r"\bum\b", s, re.IGNORECASE)
+    return len(match)
+
+if __name__ == "__main__":
+    main()
